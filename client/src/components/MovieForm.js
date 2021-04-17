e3c317c275b8ca2b6fd37887513b8d843e13d3df
devon <dev206@gmail.com>
2021-04-17 10:57:06 -0700

    commiting client code (whoops!)

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,7 +2,8 @@ import './MovieForm.css';
 import { navigate } from '@reach/router';
 import {useState} from 'react';
 const getFormReadyDate = (date=null) => {
-    return new Date(date).toISOString().split('T')[0];
+    const dt = (date === null) ? new Date() : new Date(date);
+    return dt.toISOString().split('T')[0];
 }
 const INITIAL_MOVIE_STATE = {
     title: '',
@@ -10,13 +11,13 @@ const INITIAL_MOVIE_STATE = {
     releaseDate: getFormReadyDate(),
     rating: ''
 }
-const RATINGS = ['G', 'PG', 'PG-13', 'R', 'NC-17', 'Unrated'];
+// const RATINGS = ['G', 'PG', 'PG-13', 'R', 'NC-17', 'Unrated'];
 const MovieForm = (props) => {
+    console.log("rendering movie form");
     const initialState = (props.movie === undefined)
         ? INITIAL_MOVIE_STATE
         : {...props.movie, releaseDate:getFormReadyDate(props.movie.releaseDate)};
-    const { action } = props;
-    const {onSubmitProp} = props;
+    const { errors, onSubmitProp } = props;
     const [movie, setMovie] = useState({...initialState});
     const {title, genre, releaseDate, rating} = movie;
     function onInputChanged(field, value) {
@@ -25,25 +26,29 @@ const MovieForm = (props) => {
     function onSubmitHandler(e) {
         e.preventDefault();
         onSubmitProp(movie);
-        setMovie({...INITIAL_MOVIE_STATE});
-        navigate('/');
+        if(errors.length < 1) { setMovie({...INITIAL_MOVIE_STATE}); }
+        //navigate('/');
     }
     return (
         <form onSubmit={(e) => onSubmitHandler(e)}>
             <div className="form-group">
                 <label htmlFor="title">Title</label>
+                <span className="error">{ errors?.title?.message }</span>
                 <input className="form-control" type="text" id="title" value={title} onChange={(e) => onInputChanged('title', e.target.value)} />
             </div>
             <div className="form-group">
                 <label htmlFor="genre">Genre</label>
+                <span className="error">{ errors?.genre?.message }</span>
                 <input className="form-control" type="text" id="genre" value={genre} onChange={(e) => onInputChanged('genre', e.target.value)} />
             </div>
             <div className="form-group">
                 <label htmlFor="releaseDate">Release Date</label>
+                <span className="error">{ errors?.releaseDate?.message }</span>
                 <input className="form-control" type="date" id="releaseDate" value={releaseDate} onChange={(e) => onInputChanged('releaseDate', e.target.value)} />
             </div>
             <div className="form-group">
                 <label htmlFor="rating">Rating</label>
+                <span className="error">{ errors?.rating?.message }</span>
                 <input className="form-control" type="text" id="rating" value={rating} onChange={(e) => onInputChanged('rating', e.target.value)} />
             </div>
             <button type="submit" className="btn btn-primary">Send</button>
